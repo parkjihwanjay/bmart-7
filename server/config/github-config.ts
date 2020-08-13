fb0b4d6785a725334095816a3cbc86ad99ce0278
parkjihwanjay <cc6656@maver.com>
2020-08-13 14:57:44 +0900

    [feature][#6] github-config.ts 수정사항 반영
    - baseUrl을 localhost:4000으로 고정

--- server/config/github-config.ts
+++ server/config/github-config.ts
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NODE_ENV == 'dev' ? 'http://localhost:4000' : ''
+const baseUrl = 'http://localhost:4000'
 export const githubLoginUrl = 'https://github.com/login/oauth/authorize'
 export const githubTokenUrl = 'https://github.com/login/oauth/access_token'
 export const githubCallbackUrl = `${baseUrl}/api/github-login/callback`
