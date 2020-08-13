46730d5080f65389f7ba1fb12c393bf53704c74b
parkjihwanjay <cc6656@maver.com>
2020-08-13 16:19:02 +0900

    [update][#6] jwt.ts 수정
    - jwt option을 변수화
    - 함수 인자와 return 형식을 typed으로 정형화
      - IDecoded : decodeJwt의 return 형식
      - IGithubUser : encodeJwt의 인자 형식

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -5,22 +5,38 @@ import path from 'path'
 if (process.env.NODE_ENV == 'test')
   dotenv.config({ path: path.join(__dirname, '../.env.test') })
 
-export const encodeJwt = async githubUser => {
+type IDecoded = {
+  id: number
+  username: string
+  iat?: number
+  exp?: number
+  iss?: string
+  sub?: string
+}
+
+type IGithubUser = {
+  login: string
+  id: number
+}
+
+const jwtOption = {
+  expiresIn: '30d',
+  issuer: 'jihwan',
+  subject: 'userInfo'
+}
+
+export const encodeJwt = async (githubUser: IGithubUser): Promise<string> => {
   const token = await jwt.sign(
     {
       id: githubUser.id,
       username: githubUser.login
     },
     process.env.JWT_SECRET,
-    {
-      expiresIn: '30d',
-      issuer: 'jihwan',
-      subject: 'userInfo'
-    }
+    jwtOption
   )
   return token
 }
 
-export const decodeJwt = async token => {
+export const decodeJwt = async (token: string): Promise<IDecoded> => {
   return await jwt.verify(token, process.env.JWT_SECRET)
 }
