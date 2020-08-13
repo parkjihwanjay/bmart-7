9365583cea21f82d08bfdb26c06bbd3b25e4bae9
blair B <blair441035@gmail.com>
2020-08-13 17:11:06 +0900

    [test][#9]formatting

--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -2,35 +2,30 @@ import React from 'react'
 import { useQuery } from 'react-apollo'
 import { GET_USERS } from './gql'
 
-import './login.scss';
+import './login.scss'
 import { FaGithubSquare } from 'react-icons/fa'
-import { FaFacebookSquare } from 'react-icons/fa';
-
-
+import { FaFacebookSquare } from 'react-icons/fa'
 
 export const Login = () => {
-
   return (
-    <div className="login-section"> 
-        {/* <image src={process.env.PUBLIC_URL + "/images/bmartlogo.jpeg"}/> */}
-        <img className="logo" src='./images/bmartlogo.jpeg'/>
+    <div className="login-section">
+      {/* <image src={process.env.PUBLIC_URL + "/images/bmartlogo.jpeg"}/> */}
+      <img className="logo" src="./images/bmartlogo.jpeg" />
       <div className="social-login-wrap">
         <div className="login-btn-wrap">
-        <FaGithubSquare size={50}/>
-        <div className="login-text">Login woth Github</div>
+          <FaGithubSquare size={50} />
+          <div className="login-text">Login woth Github</div>
         </div>
 
         <div className="login-btn-wrap">
-        <FaFacebookSquare size={50}/>
-        <div className="login-text">Login woth Facebook</div>
+          <FaFacebookSquare size={50} />
+          <div className="login-text">Login woth Facebook</div>
         </div>
         <div className="social-login-title">Click SocialLogin</div>
       </div>
     </div>
   )
 }
-
-
 
 // export const Login = () => {
 //   const { loading, error, data } = useQuery(GET_USERS)
