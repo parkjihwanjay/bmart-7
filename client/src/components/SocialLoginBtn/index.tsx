b85ff45dc87dde7a0e2a1e04ed92dd4a24ca2224
parkjihwanjay <cc6656@maver.com>
2020-08-15 01:44:31 +0900

    [update][#32] SocialLoginBtn 컴포넌트 리팩토링
    - props의 타입을 Iprops로 정의
    - props로 전달받은 type에 따라 Icon Component을 반환하는 함수 생성
    - event가 아닌 a태그로 github 로그인 창 이동
    - props 비구조 할당을 통한 리팩토링
    - 해당 컴포넌트는 상위 컴포넌트에서 전달받은 데이터를 렌더링에 집중하도록 수정

--- client/src/components/SocialLoginBtn/index.tsx
+++ client/src/components/SocialLoginBtn/index.tsx
@@ -1,42 +1,30 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './socialLoginBtn.scss'
 import { FaGithubSquare } from 'react-icons/fa'
 import { FaFacebookSquare } from 'react-icons/fa'
 
-import axios from 'axios'
-import queryParser from 'query-parser-url'
-const baseUrl = 'http://localhost:4000'
-const githubLoginUrl = 'https://github.com/login/oauth/authorize'
-const githubCallbackUrl = `http://localhost:3000`
-const GITHUB_CLIENT_ID = '9368cfd68ad4ee3c646e'
+type IProps = {
+  icon : string
+  text : string
+  href : string
+}
 
-export const SocialLoginBtn = (props: { icon: string; text: string }) => {
-  useEffect(() => {
-    const url = window.location.search.substring(1)
-
-    const queryObject = queryParser(url)
-    const code = queryObject['code']
-    if (!code) return
-
-    axios.get(`/api/github-login/?code=${code}`).then(res => {
-      localStorage.setItem('token', res.data.token)
-    })
-  })
-  const githubLoginHandler = async e => {
-    if (props.icon === 'github') {
-      location.href = `https://github.com/login/oauth/authorize/?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${githubCallbackUrl}`
-      return
-    }
+const getIcon = (type : string) => {
+  switch(type){
+    case 'github':
+      return <FaGithubSquare size={50} />
+    case 'facebook':
+      return <FaFacebookSquare className="facebook" size={50} />
   }
-
+}
+export const SocialLoginBtn = (props: IProps) => {
+  const {icon, href, text} = props
   return (
-    <button className="login-btn-wrap" onClick={githubLoginHandler}>
-      {props.icon === 'github' ? (
-        <FaGithubSquare size={50} />
-      ) : (
-        <FaFacebookSquare className="facebook" size={50} />
-      )}
-      <div className="login-text">{props.text}</div>
-    </button>
+    <a href={href}>
+      <button className="login-btn-wrap">
+        {getIcon(icon)}
+        <div className="login-text">{text}</div>
+      </button>
+    </a>
   )
 }
