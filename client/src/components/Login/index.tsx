3726ec2534ff1188448f41ae006244a046aa6be4
parkjihwanjay <cc6656@maver.com>
2020-08-15 01:47:36 +0900

    [update][#32] front-Login 컴포넌트 수정
    - 로그인 이후 로직 처리 : useEffect 사용
    - socialSites 정보를 돌면서 SocialLoginButton의 배열을 만들어 렌더링
    - socialSites는 social Login을 사용하는 사이트의 정보로 ./config.ts에 포함되어 있다.

--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { SocialLoginBtn } from '../SocialLoginBtn/index'
 import './login.scss'
+import axios from 'axios'
+import queryParser from 'query-parser-url'
+import {socialSites} from './config'
 
 export const Login = () => {
+  useEffect(() => {
+    const url = window.location.search.substring(1)
+
+    const queryObject = queryParser(url)
+    const code = queryObject['code']
+    if (!code) return
+
+    axios.get(`/api/github-login/?code=${code}`).then(res => {
+      localStorage.setItem('token', res.data.token)
+    })
+  }, []);
+
+  const socialLoginBtnList = socialSites.map((site) => {
+    return <SocialLoginBtn icon={site.icon} text={site.text} href={site.href}/>
+  })
+
   return (
     <div className="login-section">
       <img className="logo" src="./images/bmartlogo.jpeg" />
       <div className="social-login-wrap">
-        <SocialLoginBtn icon="github" text="Login with Github" />
-        <SocialLoginBtn icon="facebook" text="Login with Facebook" />
+        {socialLoginBtnList}
         <div className="social-login-title">Click SocialLogin</div>
       </div>
     </div>
