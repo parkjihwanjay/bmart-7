43797b9b0417fe0964bd62942143afc6e89b752b
parkjihwanjay <cc6656@maver.com>
2020-08-16 04:40:37 +0900

    [env][#38] front-env
    - github client id와 base url을 환경변수 화
    - .env.development와 .env.production 파일 생성
    - REACT_APP_으로 시작하면 react가 알아서 환경변수로 인식
    - npm run start -> .env.development 파일 적용
    - npm run build -> .env.production 파일 적용

--- client/src/components/Login/config.ts
+++ client/src/components/Login/config.ts
@@ -1,11 +1,11 @@
-const GITHUB_CLIENT_ID = '9368cfd68ad4ee3c646e'
-const githubCallbackUrl = `http://localhost:3000`
+const GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 export const socialSites = [
   {
     icon : 'github',
     text : 'Login with Github',
-    href : `https://github.com/login/oauth/authorize/?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${githubCallbackUrl}`
+    href : `https://github.com/login/oauth/authorize/?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${BASE_URL}/login`
   },
   {
     icon : 'facebook',
