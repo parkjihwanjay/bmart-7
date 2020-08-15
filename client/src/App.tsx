c4b1069e2d10b78573f5f1e8d3ef5cd04347a40e
parkjihwanjay <cc6656@maver.com>
2020-08-16 04:38:03 +0900

    [feature][#38] front-router 적용
    - App.ts에 Router 적용
    - /login이면 로그인 컴포넌트로 이동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { client } from './ApolloClient'
 import { ApolloProvider } from 'react-apollo'
 import { LoginPage } from './pages/LoginPage'
@@ -10,9 +11,15 @@ import './styles/fonts.scss'
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="app">
-        <LoginPage />
-      </div>
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </ApolloProvider>
   )
 }
