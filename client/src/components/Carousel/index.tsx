6aafff3c9f3ec836581b4a54f100ae4e3998aa33
Rachel4858 <rachel458458@gmail.com>
2020-08-16 22:31:52 +0900

    [feat][#33]Carousel tag생성,스타일 적용,image mockdate배열로 생성

--- client/src/components/Carousel/index.tsx
+++ client/src/components/Carousel/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './style.scss'
+import { FiArrowLeftCircle } from 'react-icons/fi'
+import { FiArrowRightCircle } from 'react-icons/fi'
 
-const imgUrlArr: any = [
+const imgUrlArr: Array<string> = [
   'https://cdn.pixabay.com/photo/2018/09/24/03/05/cat-3699032__480.jpg',
   'https://cdn.pixabay.com/photo/2017/08/07/18/57/dog-2606759__480.jpg',
   'https://cdn.pixabay.com/photo/2015/02/25/17/56/cat-649164__480.jpg',
@@ -10,11 +12,22 @@ const imgUrlArr: any = [
 ]
 
 export const Carousel: React.FC = () => {
-  console.log(imgUrlArr, 'saas')
+  const clickLeftBtn = (e) => {
+    console.log(e.target)
+  }
 
+  const clickLRightBtn = () => {
+    console.log('rr')
+  }
   return (
     <div className="carousel">
-      <img alt="cat" src={imgUrlArr[0]} />
+      <div className="images">
+        {imgUrlArr.map((url: string, idx: number) => (
+          <img data-id={idx} src={url} alt="cat" />
+        ))}
+      </div>
+      <FiArrowLeftCircle className="left-arrow-btn" size={20} onClick={clickLeftBtn} />
+      <FiArrowRightCircle className="right-arrow-btn" size={20} onClick={clickLRightBtn} />
     </div>
   )
 }
