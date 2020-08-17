827567b94cf27b5c3e2fa2d8db7194282f32f253
Choibumsu <bumsu0211@gmail.com>
2020-08-17 12:03:51 +0900

    [feat][#47] 찜하기 기능 API 작업 및 리팩토링
    - getUserFavorite : 유저가 찜한 상품들을 반환
    - inserFavorite : 찜한 상품을 추가
    - deleteFavorite : 찜한 상품을 삭제
    - ApolloError : GraphQL은 http status code를 사용하지 않으므로, ApolloError를 통해 메세지와 코드를 전달

--- server/graphql/user/user-resolver.ts
+++ server/graphql/user/user-resolver.ts
@@ -2,37 +2,34 @@ import { Context } from '../context'
 import {
   UserWhereUniqueInput,
   UserWhereInput,
-  UserCreateInput
+  UserCreateInput,
+  FavoriteWhereUniqueInput,
 } from '@prisma/client'
+import { ApolloError } from 'apollo-server-express'
 
 export const userResolver = {
   Query: {
     getUserById,
-    getUsers
+    getUsers,
+    getUserFavorites,
   },
   Mutation: {
-    insertUser
-  }
+    insertUser,
+    insertFavorite,
+    deleteFavorite,
+  },
 }
 
-async function getUserById(
-  parent,
-  args: { input: UserWhereUniqueInput },
-  context: Context
-) {
-  const { id } = { ...args.input }
+async function getUserById(parent, args: UserWhereUniqueInput, context: Context) {
+  const id = args.id
   return await context.prisma.user.findOne({
     where: {
-      id: id
-    }
+      id: id,
+    },
   })
 }
 
-async function getUsers(
-  parent,
-  args: { input: UserWhereInput },
-  context: Context
-) {
+async function getUsers(parent, args: { input: UserWhereInput }, context: Context) {
   const { id, userId, email, phone, address } = { ...args.input }
   return await context.prisma.user.findMany({
     where: {
@@ -40,16 +37,27 @@ async function getUsers(
       userId,
       email,
       phone,
-      address
-    }
+      address,
+    },
   })
 }
 
-async function insertUser(
-  parent,
-  args: { input: UserCreateInput },
-  context: Context
-) {
+async function getUserFavorites(parent, args: UserWhereUniqueInput, context: Context) {
+  const id = args.id
+  return await context.prisma.user
+    .findOne({
+      where: {
+        id,
+      },
+    })
+    .favorites({
+      include: {
+        product: true,
+      },
+    })
+}
+
+async function insertUser(parent, args: { input: UserCreateInput }, context: Context) {
   const { id, userId, email, phone, address } = { ...args.input }
   return await context.prisma.user.create({
     data: {
@@ -57,7 +65,47 @@ async function insertUser(
       userId,
       email,
       phone,
-      address
-    }
+      address,
+    },
+  })
+}
+
+async function insertFavorite(parent, args: { input: FavoriteWhereUniqueInput }, context: Context) {
+  const { userId, productId } = { ...args.input }
+
+  const isExist =
+    (await context.prisma.favorite.count({
+      where: {
+        productId,
+        userId,
+      },
+    })) > 0
+
+  if (isExist) {
+    throw new ApolloError('Already Exist Favorite', 'DUPLICATE')
+  }
+
+  return await context.prisma.favorite.create({
+    data: {
+      product: {
+        connect: {
+          id: productId,
+        },
+      },
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  })
+}
+
+async function deleteFavorite(parent, args: FavoriteWhereUniqueInput, context: Context) {
+  const id = args.id
+  return await context.prisma.favorite.delete({
+    where: {
+      id,
+    },
   })
 }
