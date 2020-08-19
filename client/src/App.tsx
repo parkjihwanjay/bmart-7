f6516ebc186d444873b95a034d6ed70006a48624
Choibumsu <bumsu0211@gmail.com>
2020-08-19 10:59:33 +0900

    [feat][#48] global store 작업
    - global store 자체와 setState 함수를 각각 컨텍스트로 생성
    - global로 사용하기 위해 app을 컨텍스트로 감쌈

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { client } from './ApolloClient'
 import { ApolloProvider } from 'react-apollo'
 import { LoginPage } from '@/pages/LoginPage'
+import { FavoritePage } from '@/pages/FavoritePage'
 import '@/styles/reset.scss'
 import '@/styles/base.scss'
 import '@/styles/fonts.scss'
+import { StoreContext, SetStoreContext, StoreType, globalStore } from '@/store'
 
 function App() {
+  const [store, setStore] = useState<StoreType>(globalStore)
+
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <div id="app">
-          <Switch>
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <StoreContext.Provider value={store}>
+        <SetStoreContext.Provider value={setStore}>
+          <Router>
+            <div id="app">
+              <Switch>
+                <Route path="/login">
+                  <LoginPage />
+                </Route>
+              </Switch>
+              <Switch>
+                <Route path="/favorite">
+                  <FavoritePage />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </SetStoreContext.Provider>
+      </StoreContext.Provider>
     </ApolloProvider>
   )
 }
