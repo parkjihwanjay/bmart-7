aa904fb8008f0bb430b24cddd617bf561e299f35
blair B <blair441035@gmail.com>
2020-08-21 00:41:17 +0900

    [feat][#71]PreviewContainer작업: - PreviewContainer Component생성 및 태그 작성 - style 작업

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -3,12 +3,14 @@ import { SlickCarousel } from '@/components/common/Carousel'
 import { MainCategoryList } from '@/components/MainPage/MainCategoryList'
 import { RecommendedContainer } from '@/components/RecommendedContainer'
 import './style.scss'
+import { PreviewContainer } from '@/components/MainPage/PreviewContainer'
 
 export const MainPage: React.FC = () => {
   return (
     <div id="main-page">
       <SlickCarousel />
       <MainCategoryList />
+      <PreviewContainer />
       <RecommendedContainer title="지금 머먹지" categoryId={187} totalPageNum={3} />
       <RecommendedContainer title="지금 필요한 생필품!" categoryId={187} totalPageNum={3} />
     </div>
