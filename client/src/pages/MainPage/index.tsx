63f2cbc5d33ff159247cf8366fc500862fb98c52
parkjihwanjay <cc6656@maver.com>
2020-08-20 18:33:27 +0900

    [feat][#64] front-CategoryPreview MainPage에 추가

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SlickCarousel } from '@/components/Carousel/index.tsx'
 import { RecommendedContainer } from '@/components/RecommendedContainer'
+import {CategoryPreview} from '@/components/CategoryPreview'
 import './style.scss'
 
 export const MainPage: React.FC = () => {
@@ -9,6 +10,7 @@ export const MainPage: React.FC = () => {
       <SlickCarousel />
       <RecommendedContainer title="지금 머먹지" categoryId={187} totalPageNum={3}/>
       <RecommendedContainer title="지금 필요한 생필품!" categoryId={187} totalPageNum={3}/>
+      <CategoryPreview title="밀키트" categoryId={187}></CategoryPreview>
     </div>
   )
 }
