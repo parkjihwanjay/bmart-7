be8e6938d0267c2d987cf54abb08692104d6615a
parkjihwanjay <cc6656@maver.com>
2020-08-20 15:46:42 +0900

    [feat][#55] RecommendedConatiner Component 추가
    - props로 categoryId, title, totalPageNum을 받음
    - 페이지가 렌더링 될때마다 useQuery hook을 이용해 6개의 isMain 상품을 가져옴
    - cache 정책을 통해 이미 가져온 데이터는 다시 요청하지 않도록
    - ProductList 재사용
    - 원래는 fetchMore 함수를 통해 기존의 데이터에 계속 추가하는 식으로 구현을 하려고 했으나 현재 페이지를 state로 가지는데 그러면 rerender가 너무 많이 일어나서 cache 정책으로 수정(논의 필요)

--- client/src/components/RecommendedContainer/index.tsx
+++ client/src/components/RecommendedContainer/index.tsx
@@ -1,27 +1,48 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import './style.scss'
 import { StoreContext, SetStoreContext } from '@/store'
-import {Product} from '@/types'
-
-interface IProps{
-    title : string
+import { ProductList } from '@/components/common/ProductList'
+import { GET_RECOMMENDED } from './gql'
+import { useQuery } from 'react-apollo'
+import {BsArrowCounterclockwise} from 'react-icons/bs'
+interface IProps {
+  title: string
+  categoryId : number
+  totalPageNum : number
 }
 
 export const RecommendedContainer: React.FC<IProps> = (props) => {
-    const { title } = props
+  const limit = 6
+  const [pageNum, setPageNum] = useState(1)
+  const offset = (pageNum-1)*limit
+  const { title, categoryId, totalPageNum } = props
+  const store = useContext(StoreContext)
+  const favoriteList = store.favorites.map((favorite) => favorite.product)
+  const { loading, error, data, fetchMore } = useQuery(GET_RECOMMENDED, {
+    variables : {
+      offset,
+      limit,
+      categoryId
+    },
+    fetchPolicy: "cache-and-network"
+  })
 
-    const store = useContext(StoreContext)
-    const productList = store.favorites.map((favorite) => favorite.product)
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error...</p>
 
-    useEffect(() => {
-        // 상품 6개를 가져오는 로직 추가
-    }, [])
+  const productList = data.getRecommended
 
-    return (
-        <div className='recommended-container'>
-            <h2>{title}</h2>
-            {/* <ProductList column={3} productList={productList} /> */}
-            <input type="button" value="지금 머 먹지?"/>
-        </div>
-    )
+  function onClickHandler(e){
+    setPageNum(pageNum+1)
+  }
+
+  return (
+    <div className="recommended-container">
+      <h2>{title}</h2>
+      <ProductList column={3} productList={productList} />
+      <button onClick={onClickHandler}>
+        <span><BsArrowCounterclockwise className="title-icon"/>{title}?</span> 다른 상품 보기 {pageNum}/{totalPageNum}
+      </button>
+    </div>
+  )
 }
