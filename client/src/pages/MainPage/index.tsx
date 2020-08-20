85f4b1971989a31e8032056a93aeea6513e6a5fe
parkjihwanjay <cc6656@maver.com>
2020-08-20 15:47:50 +0900

    [feat][#55] MainPage에 RecommendedConatiner 추가
    - title, categoryId, totaPageNum을 props로 전달
    - 일단은 categoryId는 Main이 아닌 subCategoryId로 입력

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { SlickCarousel } from '@/components/Carousel/index.tsx'
+import { RecommendedContainer } from '@/components/RecommendedContainer'
 import './style.scss'
 
 export const MainPage: React.FC = () => {
   return (
     <div id="main-page">
       <SlickCarousel />
+      <RecommendedContainer title="지금 머먹지" categoryId={187} totalPageNum={3}/>
+      <RecommendedContainer title="지금 필요한 생필품!" categoryId={187} totalPageNum={3}/>
     </div>
   )
 }
