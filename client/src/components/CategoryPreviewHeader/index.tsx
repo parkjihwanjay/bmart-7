a2cbfd3294fbe6e1227e86b36c8ad2e281989e35
parkjihwanjay <cc6656@maver.com>
2020-08-21 13:34:59 +0900

    [feat][#63] CategoryPreviewHeader 컴포넌트 구현
    - mainCategoryList를 prop로 받아 렌더링
    - scroll 이벤트를 통해 내려가면 화면 위에 붙게끔 수정
      - requestAnimation을 통한 event debouncing
    - 첫번째 카테고리일 경우 처음에 선택되게끔 설정
    - categoryId를 state를 두고 해당 categoryId를 가진 태그에 스타일 적용하도록 설정

---
+++ client/src/components/CategoryPreviewHeader/index.tsx
@@ -0,0 +1,51 @@
+import React,{useState, useRef, useEffect} from 'react'
+import './style.scss'
+import {GET_MAIN_CATEGORIES} from './gql'
+import { useQuery } from 'react-apollo'
+
+interface IProps {
+  mainCategoryList : []
+}
+
+export const CategoryPreviewHeader: React.FC<IProps> = (props) => {
+  const {mainCategoryList} = props
+
+  const [isSticky, setSticky] = useState(false);
+  const [categoryId, setCategoryId] = useState(16)
+
+  const ref = useRef(null)
+
+  let timeout
+
+  const handleScroll = () => {
+    if(timeout){
+      window.cancelAnimationFrame(timeout)
+    }
+    timeout = window.requestAnimationFrame(function () {
+      if (ref.current) {
+        setSticky(ref.current.getBoundingClientRect().top <= 0);
+      }
+    });
+    
+  };
+  
+  
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+  }, []);
+
+  
+
+  return (
+    <div className={`category-header ${isSticky ? ' sticky' : ''}`} ref={ref}>
+      {mainCategoryList.map((category, idx) => {
+      const {title, id} = category
+      return id===categoryId ? <a className="active" data-category={id} href={`#catgory-${id}`} key={id} onClick={(e) => {setCategoryId(id)}}> {title}</a> : <a href={`#catgory-${id}`} key={id} onClick={(e) => {setCategoryId(id)}}>{title}</a>
+      })}
+    </div>
+  )
+}
