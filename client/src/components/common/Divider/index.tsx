a9716ad5e8a54620d5f4cdb636936e0f0560946f
Choibumsu <bumsu0211@gmail.com>
2020-08-21 13:24:34 +0900

    [feat][#73] Divider 컴포넌트 스타일 작업
    - divider 컴포넌트 그림자 적용
    - MainPage 필요한 곳에 사용

--- client/src/components/common/Divider/index.tsx
+++ client/src/components/common/Divider/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './style.scss'
 
 export type DividerProps = {
-  color?: string
+  backgroundColor?: string
+  height?: number
+  paddingTop?: number
+  paddingBottom?: number
 }
 
 export const Divider: React.FC<DividerProps> = (props) => {
-  return <div className="divider"></div>
+  const { backgroundColor, height, paddingTop, paddingBottom } = props
+  const barElement = useRef<HTMLDivElement>()
+
+  if (backgroundColor) barElement.current.style.backgroundColor = backgroundColor
+  if (height) barElement.current.style.height = `${height}px`
+  if (paddingTop) barElement.current.style.paddingTop = `${paddingTop}px`
+  if (paddingBottom) barElement.current.style.paddingBottom = `${paddingBottom}px`
+
+  return (
+    <div className="divider">
+      <div className="bar" ref={barElement}></div>
+    </div>
+  )
 }
--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -45,13 +45,17 @@ export const MainPage: React.FC = () => {
         title="김영지님을 위해 준비한 상품"
         moreLink=""
       />
+      <Divider />
       <RecommendedContainer title="지금 머먹지" categoryId={187} totalPageNum={3} />
+      <Divider />
       <ProductSlide
         productList={newestResponse.data.getProducts}
         title="새로 나왔어요"
         moreLink=""
       />
+      <Divider />
       <RecommendedContainer title="지금 필요한 생필품!" categoryId={187} totalPageNum={3} />
+      <Divider />
       <CategoryPreview title="밀키트" categoryId={187}></CategoryPreview>
     </div>
   )
