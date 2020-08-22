e84b38b417340ce9e9e28a7238c46757f2842841
parkjihwanjay <cc6656@maver.com>
2020-08-22 14:06:55 +0900

    [feat][#63] CategoryPreviewSectionHeader 수정
    - sticky는 css만으로 구현
    - 메뉴에서 카테고리를 클릭했을때 반이상 넘어가는 메뉴에 대해서는 스크롤 이벤트가 일어나게끔 구현

--- client/src/components/CategoryPreviewHeader/index.tsx
+++ client/src/components/CategoryPreviewHeader/index.tsx
@@ -1,48 +1,50 @@
-import React,{useState, useRef, useEffect} from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './style.scss'
 
 interface IProps {
-  mainCategoryList : []
+  mainCategoryList: []
 }
 
 export const CategoryPreviewHeader: React.FC<IProps> = (props) => {
-  const {mainCategoryList} = props
+  const { mainCategoryList } = props
+  const headerRef: {
+    current: HTMLDivElement
+  } = useRef()
+  const [categoryId, setCategoryId] = useState(17)
+  const [index, setIndex] = useState(0)
 
-  const [isSticky, setSticky] = useState(false);
-  const [categoryId, setCategoryId] = useState(16)
-
-  const ref = useRef(null)
-
-  let timeout
-
-  const handleScroll = () => {
-    if(timeout){
-      window.cancelAnimationFrame(timeout)
-    }
-    timeout = window.requestAnimationFrame(function () {
-      if (ref.current) {
-        setSticky(ref.current.getBoundingClientRect().top <= 0);
-      }
-    });
-    
-  };
-  
-  
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', () => handleScroll);
-    };
-  }, []);
-
-  
-
+  const onClickHandler = ({ e, id, idx }) => {
+    const $header = headerRef.current
+    $header.scrollLeft = e.target.offsetLeft - $header.offsetWidth / 2 + e.target.offsetWidth / 2
+    setCategoryId(id)
+    setIndex(idx)
+  }
   return (
-    <div className={`category-header ${isSticky ? ' sticky' : ''}`} ref={ref}>
+    <div className="category-header sticky" ref={headerRef}>
       {mainCategoryList.map((category, idx) => {
-      const {title, id} = category
-      return id===categoryId ? <a className="active" data-category={id} href={`#catgory-${id}`} key={id} onClick={(e) => {setCategoryId(id)}}> {title}</a> : <a href={`#catgory-${id}`} key={id} onClick={(e) => {setCategoryId(id)}}>{title}</a>
+        const { title, id } = category
+        return id === categoryId ? (
+          <a
+            className="active"
+            data-category={id}
+            href={`#catgory-${id}`}
+            key={id}
+            onClick={(e) => onClickHandler({ e, id, idx })}
+          >
+            {title}
+          </a>
+        ) : (
+          <a
+            href={`#catgory-${id}`}
+            data-category={id}
+            key={id}
+            onClick={(e) => {
+              onClickHandler({ e, id, idx })
+            }}
+          >
+            {title}
+          </a>
+        )
       })}
     </div>
   )
